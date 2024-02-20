6859839b1af4e01078c5e1e1eb0eda377b5a3c3e
username <wecp-dummy@gmail.com>
2024-02-20 07:07:36 +0000

    Final

--- frontend/src/app/auth/components/login/login.component.ts
+++ frontend/src/app/auth/components/login/login.component.ts
@@ -6,52 +6,52 @@ import { Router } from "@angular/router";
 import { AuthService } from "../../services/auth.service";
 
 @Component({
-  selector: "app-login",
-  templateUrl: "./login.component.html",
-  styleUrls: ["./login.component.css"],
+    selector: "app-login",
+    templateUrl: "./login.component.html",
+    styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
-  loginForm: FormGroup;
-  loginError$: Observable<{ [key: string]: string }>;
+    loginForm: FormGroup;
+    loginError$: Observable<{ [key: string]: string }>;
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private authService: AuthService,
-    private router: Router
-  ) {}
+    constructor(
+        private formBuilder: FormBuilder,
+        private authService: AuthService,
+        private router: Router
+    ) { }
 
-  ngOnInit(): void {
-    this.loginForm = this.formBuilder.group({
-      username: ["", [Validators.required]],
-      password: ["", Validators.required],
-     
-    });
-  }
+    ngOnInit(): void {
+        this.loginForm = this.formBuilder.group({
+            username: ["", [Validators.required]],
+            password: ["", Validators.required],
 
-  onSubmit(): void {
-    if (this.loginForm.invalid) {
-      this.loginError$ = of({
-        message: "Please make sure you have filled all the required fields",
-      });
-      return;
-    } else {
-      const { username, password } = this.loginForm.value;
-      this.loginError$ = this.authService
-        .login({ username, password })
-        .pipe(
-          tap((response) => {
-            console.log(response);
-            localStorage.setItem("token", response.token);
-            localStorage.setItem("role",response.roles);
-            localStorage.setItem("user_id",response.userId);
-            console.log(localStorage.getItem("role"));
-            this.router.navigate(["bank"]);
-          }),
-          catchError((error) => {
-            console.error("Login error:", error);
-            return of({ message: "Login error:"+ error });
-          })
-        );
+        });
     }
-  }
+
+    onSubmit(): void {
+        if (this.loginForm.invalid) {
+            this.loginError$ = of({
+                message: "Please make sure you have filled all the required fields",
+            });
+            return;
+        } else {
+            const { username, password } = this.loginForm.value;
+            this.loginError$ = this.authService
+                .login({ username, password })
+                .pipe(
+                    tap((response) => {
+                        console.log(response);
+                        localStorage.setItem("token", response.token);
+                        localStorage.setItem("role", response.roles);
+                        localStorage.setItem("user_id", response.userId);
+                        console.log(localStorage.getItem("role"));
+                        this.router.navigate(["bank"]);
+                    }),
+                    catchError((error) => {
+                        console.error("Login error:", error);
+                        return of({ message: "Login error: Incorrect Username & Password"});
+                    })
+                );
+        }
+    }
 }
